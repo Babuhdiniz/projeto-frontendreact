fe2f98cc6679d44c082ec4095aa926ddbbdcd207
Babuhdiniz <babuhdiniz@gmail.com>
2023-07-02 15:24:30 -0300

    [estilo] implementei a estilizacão básica com styled-components

--- projetoreact/src/App.js
+++ projetoreact/src/App.js
@@ -1,20 +1,27 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 import Filters from "./Components/Filters/Filters";
 import Home from "./Components/ProductList/Home/Home";
 import ProductCard from "./Components/ProductList/ProductCard/ProductCard";
 import Cart from "./Components/ShoppingCart/Cart/Cart";
 import Items from "./Components/ShoppingCart/Items/Items";
+import GlobalStyles from "./globalStyles";
+
 
 function App() {
   return (
-    <div>
-      <Filters />
-      <Home />
-      <ProductCard />
-      <Cart />
-      <Items />
-    </div>
+    <>
+      {/* <GlobalStyles /> */}
+      <div className="estilo">
+        <Filters />
+        <Home />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <Cart />
+        {/* <Items /> */}
+      </div>
+    </>
   );
 }
 
--- projetoreact/src/Components/ShoppingCart/Cart/Cart.js
+++ projetoreact/src/Components/ShoppingCart/Cart/Cart.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Cart = () => {
   return (
     <div>
-      <p>Cart</p>
+      <h1>Cart</h1>
+      <p>
+        x0 Nome do produto <button> Remover </button>
+        Valor total: 0
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Cart
+export default Cart;
